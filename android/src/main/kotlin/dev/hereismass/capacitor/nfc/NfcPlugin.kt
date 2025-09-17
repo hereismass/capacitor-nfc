2c572c8e644c9ce552ee9d4e14ea80d9c9519036
Massimo Zambelli <mass.zambelli@gmail.com>
2025-09-17 15:00:29 +0200

    upgrade gradle for example app

---
+++ android/src/main/kotlin/dev/hereismass/capacitor/nfc/NfcPlugin.kt
@@ -0,0 +1,21 @@
+package dev.hereismass.capacitor.nfc
+
+import com.getcapacitor.JSObject
+import com.getcapacitor.Plugin
+import com.getcapacitor.PluginCall
+import com.getcapacitor.PluginMethod
+import com.getcapacitor.annotation.CapacitorPlugin
+
+@CapacitorPlugin(name = "Nfc")
+class NfcPlugin : Plugin() {
+    private val implementation = Nfc()
+
+    @PluginMethod
+    fun echo(call: PluginCall) {
+        val value = call.getString("value")
+
+        val ret = JSObject()
+        ret.put("value", implementation.echo(value))
+        call.resolve(ret)
+    }
+}
